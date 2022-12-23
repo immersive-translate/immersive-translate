df7557a0818bf5772bb1f9f992535f7fd4cb9aef
Owen <62473795+theowenyoung@users.noreply.github.com>
2022-12-23 02:33:21 +0000

    Update immersive-translate.user.js

--- docs/immersive-translate.user.js
+++ docs/immersive-translate.user.js
@@ -1,3 +1,23 @@
+// ==UserScript==
+// @name         Next Immersive Translate
+// @namespace    https://immersive-translate.owenyoung.com/
+// @version      0.0.5.1
+// @description  Web page translator
+// @author       Owen Young
+// @match      *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=userscript.net
+// @grant       GM.getValue
+// @grant       GM.setValue
+// @grant       GM.xmlHttpRequest
+// @grant       GM.registerMenuCommand
+// @connect    translate.googleapis.com
+// @connect    transmart.qq.com
+// @connect    tmt.tencentcloudapi.com
+// @connect    www2.deepl.com
+// @run-at       document-end
+// @require    immersive-translate.lib.js
+// ==/UserScript==
+
 globalThis.IMMERSIVE_TRANSLATE_CONFIG = {
   debug: true,
   translationLanguagePattern: {

33f6d47ae372c47ec1718bab1ebd22f8458237c9
Owen Young <theowenyoung@gmail.com>
2023-05-06 22:24:56 +0800

    add start

---
+++ docs/start/start.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
